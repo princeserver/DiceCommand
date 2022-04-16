9aabd975613b6fb02f2060e325c8f8adff1eabd8
majisyou <majisyou1999@gmail.com>
2022-04-16 17:50:11 +0900

    HashMapによるCooldownの設定

--- src/main/java/com/github/majisyou/dice/commands/DiceCommand.java
+++ src/main/java/com/github/majisyou/dice/commands/DiceCommand.java
@@ -11,47 +11,64 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class DiceCommand implements CommandExecutor {
+    public HashMap<String, Long> cooldowns = new HashMap<String,Long>();
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
-
-        sender.getServer().getLogger().info("diceが実行されました");
-
-        if(args.length < 2) {//サブコマンドの個数が0,サブコマンド無し
-            sender.sendMessage(ChatColor.RED + "§a 引数が足りません");
-            sender.sendMessage("§a/dice <サイコロの数> <サイコロの面の数>");
-            return true;
-        }
-
-        if(!(args[1].chars().allMatch(Character::isDigit) && args[1].chars().allMatch(Character::isDigit))){
-            sender.sendMessage(ChatColor.RED+"§a 引数が間違っています。");
-            sender.sendMessage("第2,3引数には数値が必要です");
-            return true;
-        }
-
-        int times = Byte.parseByte(args[0]); //サイコロを振る回数
-        int number = Byte.parseByte(args[1]); //サイコロの面の数
-
-        List<Integer> result = Dicesystem.Dicemain(times,number);
-        List<Player> players = new ArrayList<Player>();
-        if(args.length > 2){
-            for(int i=2; i< args.length; i++){
-                if(sender.getServer().getPlayer(args[i])==null) players.add(null);
-                players.add(sender.getServer().getPlayer(args[i]).isOnline() ? sender.getServer().getPlayer(args[i]): null);
+        if(command.getName().equals("setdice")){
+            int Cooldowntime = 3;
+            if(cooldowns.containsKey(sender.getName())){
+                long secondleft = ((cooldowns.get(sender.getName())/1000)+Cooldowntime) -(System.currentTimeMillis()/ 1000);
+                if(secondleft>0){
+                    sender.sendMessage("このコマンドは3秒のcooltimeがあります");
+                    return true;
+                }
             }
-        }
+            if(args.length < 2) {//サブコマンドの個数が0,サブコマンド無し
+                sender.sendMessage(ChatColor.RED + "§a 引数が足りません");
+                sender.sendMessage("§a/dice <サイコロの数> <サイコロの面の数>");
+                return true;
+            }
 
-        for(int i=0; i<times; i++){
-            String result_message=(i+1)+"回目の結果"+result.get(i);
-            sender.sendMessage(result_message);
-            if(args.length>2){
-                for (int j=0;j<players.size();j++){
-                    if (players.get(j)!=null){
-                        players.get(j).sendMessage(result_message);
-                    } else sender.sendMessage(args[j]+"オンラインじゃないか存在しないよ");
+            if(!(args[1].chars().allMatch(Character::isDigit) && args[1].chars().allMatch(Character::isDigit))){
+                sender.sendMessage(ChatColor.RED+"§a 引数が間違っています。");
+                sender.sendMessage("第2,3引数には数値が必要です");
+                return true;
+            }
+
+            int times = Byte.parseByte(args[0]); //サイコロを振る回数
+            int number = Byte.parseByte(args[1]); //サイコロの面の数
+
+            List<Integer> result = Dicesystem.Dicemain(times,number);
+            List<Player> players = new ArrayList<Player>();
+            if(args.length > 2){
+                for(int i=2; i< args.length; i++){
+                    if(sender.getServer().getPlayer(args[i])==null) players.add(null);
+                    players.add(sender.getServer().getPlayer(args[i]).isOnline() ? sender.getServer().getPlayer(args[i]): null);
+                }
+            }
+
+            for(int i=0; i<times; i++){
+                String result_message=(i+1)+"回目の結果"+result.get(i);
+                sender.sendMessage(result_message);
+                if(args.length>2){
+                    for (int j=0;j<players.size();j++){
+                        if (players.get(j)!=null){
+                            players.get(j).sendMessage(sender.getName()+":"+result_message);
+                        } else sender.sendMessage(args[j]+"オンラインじゃないか存在しないよ");
+                    }
+                }
+            }
+            cooldowns.put(sender.getName(),System.currentTimeMillis());
+            for (String player2: cooldowns.keySet()){
+                long secondleft = ((cooldowns.get(player2)/1000)+Cooldowntime) -(System.currentTimeMillis()/ 1000);
+                if(secondleft <= 0){
+                 players.remove(player2);
                 }
             }
         }

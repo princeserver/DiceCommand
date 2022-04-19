02737bb714ae7861a1cd4fdca7cfc6032837cf68
majisyou <majisyou1999@gmail.com>
2022-04-20 01:47:52 +0900

    コンフィグをいじる

---
+++ src/main/java/com/github/majisyou/dice/system/ConfigManager.java
@@ -0,0 +1,29 @@
+package com.github.majisyou.dice.system;
+
+import com.github.majisyou.dice.Dice;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.List;
+
+public class ConfigManager {
+    private static Dice plugin = Dice.getInstance();
+    private static FileConfiguration config = plugin.getConfig();
+
+    private static String ItemType;
+    private static String displayname;
+    private static Integer customModelData;
+    private static List<String> Lore;
+
+    public static void loadConfig(){
+        ItemType = config.getString("DiceItem.ItemType");
+        displayname = config.getString("DiceItem.displayname");
+        customModelData = config.getInt("DiceItem.customModelData");
+        Lore = config.getStringList("DiceItem.Lore");
+    }
+
+    public static String getItemType(){return ItemType;}
+    public static String getDisplayname(){return displayname;}
+    public static Integer getCustomModelData(){return customModelData;}
+    public static List<String> getLore(){return Lore;}
+
+}

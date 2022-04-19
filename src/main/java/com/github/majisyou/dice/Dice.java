100763e2da726a4ef44b16b93787a28b229df230
majisyou <majisyou1999@gmail.com>
2022-04-20 01:47:06 +0900

    コンフィグを追加したから、コンフィグ系の設定

--- src/main/java/com/github/majisyou/dice/Dice.java
+++ src/main/java/com/github/majisyou/dice/Dice.java
@@ -3,17 +3,36 @@ package com.github.majisyou.dice;
 import com.github.majisyou.dice.commands.C_locommand;
 import com.github.majisyou.dice.commands.DiceCommand;
 import com.github.majisyou.dice.commands.DiceTest;
+import com.github.majisyou.dice.commands.GiveDiceCommand;
+import com.github.majisyou.dice.event.Player_CLICK;
+import com.github.majisyou.dice.system.ConfigManager;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Dice extends JavaPlugin {
+
+    private static Dice instance;
+    public Dice(){instance = this;}
+    public static Dice getInstance(){
+        return instance;
+    }
+
     @Override
     public void onEnable() {
         getLogger().info("Diceプラグインジェネレート");
         getCommand("setdice").setExecutor(new DiceCommand());
         getCommand("setdicetest").setExecutor(new DiceTest());
         getCommand("setc_lo").setExecutor(new C_locommand());
+        getCommand("give_dice").setExecutor(new GiveDiceCommand());
+
+        //config
+        saveDefaultConfig();
+        ConfigManager.loadConfig();
+
+        //event系
+        getServer().getPluginManager().registerEvents(new Player_CLICK(),this);
+
     }
 
     @Override

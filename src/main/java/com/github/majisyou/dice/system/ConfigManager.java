43e8a93fa4d8ba0ed9a8504c50ce6aadc6df35f5
majisyou <majisyou1999@gmail.com>
2022-05-04 23:27:44 +0900

    ItenTagの追加

--- src/main/java/com/github/majisyou/dice/system/ConfigManager.java
+++ src/main/java/com/github/majisyou/dice/system/ConfigManager.java
@@ -1,6 +1,7 @@
 package com.github.majisyou.dice.system;
 
 import com.github.majisyou.dice.Dice;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import java.util.List;
@@ -13,17 +14,22 @@ public class ConfigManager {
     private static String displayname;
     private static Integer customModelData;
     private static List<String> Lore;
+    private static ConfigurationSection ItemTag;
 
     public static void loadConfig(){
         ItemType = config.getString("DiceItem.ItemType");
         displayname = config.getString("DiceItem.displayname");
         customModelData = config.getInt("DiceItem.customModelData");
         Lore = config.getStringList("DiceItem.Lore");
+        ItemTag = config.getConfigurationSection("DiceItem.ItemTag");
     }
 
     public static String getItemType(){return ItemType;}
     public static String getDisplayname(){return displayname;}
     public static Integer getCustomModelData(){return customModelData;}
     public static List<String> getLore(){return Lore;}
+    public static ConfigurationSection getItemTag(){return ItemTag;}
+
+
 
 }
